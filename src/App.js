8ef189d2650a4c7e322707e6fb066e19c7dface9
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2024-12-08 22:50:31 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import Shop from "./pages/Shop"; // Import Shop Page
 import Footer from "./components/Footer";
 
 const App = () => {
   return (
-    <div className="bg-black text-white">
-      <Navbar />
-      <Home />
-      <Footer />
-    </div>
+    <Router>
+      <div className="bg-black text-white">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/shop" element={<Shop />} /> {/* Add Shop Route */}
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 };
 
