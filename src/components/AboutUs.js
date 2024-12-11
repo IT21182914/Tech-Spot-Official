8d2c9c95c0614dcbba63eeb799d6d35938c41197
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2024-12-11 17:24:49 +0530

    Button Changes

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -74,11 +74,9 @@ const AboutUs = () => {
           <div className="flex justify-center">
             <a
               href="/contact"
-              className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800"
+              className="inline-block px-6 py-3 bg-blue-500 text-white font-semibold rounded-full hover:bg-blue-600 transition duration-300"
             >
-              <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-                Contact Us
-              </span>
+              Contact Us
             </a>
           </div>
         </motion.div>
