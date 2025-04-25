d8a1d0dec226932d8a817f176ae5996a7e1b0184
Dilan Shanuka <dilanshanuka999@gmail.com>
2025-04-25 23:57:48 +0530

    Samsung

--- src/pages/Smartphones.jsx
+++ src/pages/Smartphones.jsx
@@ -44,11 +44,28 @@ const allPhones = {
   ],
   Samsung: [
     {
-      name: "Galaxy S23 Ultra",
+      name: "Samsung Galaxy S24 Ultra",
       specs: "12GB RAM | 256GB ROM",
-      image:
-        "https://cdn.pixabay.com/photo/2022/03/12/09/23/smartphone-7063761_640.jpg",
-      price: "Rs. 395,000",
+      image: "/images/Samsung/Samsung Galaxy S24 Ultra 12GB 256GB.jpg",
+      price: "Rs. 265,000",
+    },
+    {
+      name: "Samsung Galaxy S24 Ultra",
+      specs: "12GB RAM | 512GB ROM",
+      image: "/images/Samsung/Galaxy S24 ultra 12512 Gray Black 276500.jpg",
+      price: "Rs. 287,500",
+    },
+    {
+      name: "Samsung Galaxy A15 5G",
+      specs: "8GB RAM | 256GB ROM",
+      image: "/images/Samsung/Samsung Galaxy A15 5G 8GB 256GB.jpg",
+      price: "Rs. 65,000",
+    },
+    {
+      name: "Samsung Galaxy A16 5G",
+      specs: "8GB RAM | 256GB ROM",
+      image: "/images/Samsung/Galaxy A16 5G 8256 57300.jpg",
+      price: "Rs. 67,000",
     },
   ],
   Xiaomi: [
