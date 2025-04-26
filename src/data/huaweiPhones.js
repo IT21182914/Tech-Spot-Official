9691cbf5f3b58e45cf74d9c0538d2d2ad7912700
Dilan Shanuka <dilanshanuka999@gmail.com>
2025-04-26 15:13:31 +0530

    Huawei

--- src/data/huaweiPhones.js
+++ src/data/huaweiPhones.js
@@ -1,6 +1,6 @@
 export const huaweiPhones = [
   {
-    name: "Redmi Note 13",
+    name: "Huawei P50 Pro",
     specs: "8GB RAM | 128GB ROM",
     image: "/images/Redmi/Redmi Note 13 8GB 256GB.jpg",
     price: "Rs. 70,900",
